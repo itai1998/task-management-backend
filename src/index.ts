e432ccac2de49e2622b566bf4f17ad25528f4881
I-Tai Lin <smes93434@gmail.com>
2026-09-17 23:05:03 +0800

    remove unused code

--- src/index.ts
+++ src/index.ts
@@ -23,49 +23,11 @@ const testConnection = async () => {
 
 testConnection();
 
-interface Product {
-  id: number;
-  name: string;
-}
-
-const products: Product[] = [
-  { id: 1, name: "Product 1" },
-  { id: 2, name: "Product 2" },
-  { id: 3, name: "Product 3" },
-];
-
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
 app.use(taskRoutes);
-
-app.get("/", async (req: Request, res: Response) => {
-  try {
-    const result = await pool.query("SELECT * FROM tasks");
-    res.json(result.rows);
-  } catch (error) {
-    console.error("❌ Error fetching tasks:", error);
-    res.status(500).json({ message: "Internal server error" });
-  }
-});
-
-app.get("/about", (req: Request, res: Response) => {
-  res.send("About page");
-});
-
-app.get("/products", (req: Request, res: Response) => {
-  res.json(products);
-});
-
-app.get("/products/:id", (req: Request<{ id: string }>, res: Response) => {
-  const id = parseInt(req.params.id);
-  const product = products.find((p) => p.id === id);
-  if (!product) {
-    return res.status(404).json({ message: "Product not found" });
-  }
-  res.json(product);
-});
 
 app.listen(PORT, () => {
   console.log("Server is running on port 3000");
